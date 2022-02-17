6b07c3d99a666f5118c63e5ce004cf065d2c7eb8
mrideout1985 <mrideout1985@gmail.com>
2022-02-17 16:05:47 +0000

    added note service to get data from back end

---
+++ front-end/src/components/layout/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Sidebar } from "../sidebar/sidebar";
+import styles from "./layout.module.scss";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className={styles["root"]}>
+      <Sidebar />
+      <div className={styles["layout"]}>{children}</div>
+    </div>
+  );
+};
+
+export { Layout };
--- front-end/src/services/noteService.ts
+++ front-end/src/services/noteService.ts
@@ -1,4 +1,10 @@
-// class NoteService {
-//     public async getData() : Promise<any> {
-//         const response = await fetch()
-// }
+import axios from "axios";
+
+class NoteService {
+  public async getData(): Promise<any> {
+    const res = await axios.get(`http://localhost:3000/notes`);
+    return res.data;
+  }
+}
+
+export { NoteService };
