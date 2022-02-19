6d8d3c3ce600c89e27b241ddbbe935afe70e97c8
mrideout1985 <mrideout1985@gmail.com>
2022-02-19 00:49:14 +0000

    FAKE USER

--- front-end/src/components/sidebar/sidebar.tsx
+++ front-end/src/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { NavLink } from "react-router-dom"
 import { Button } from "../button/button"
 import { Title } from "../title/title"
