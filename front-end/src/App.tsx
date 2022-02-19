2223a3645e2a34bea04de523a06ff9426005aae6
mrideout1985 <mrideout1985@gmail.com>
2022-02-19 00:46:48 +0000

    added pages & routes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import "./App.scss";
-import { Layout } from "./components/layout/layout";
-import { NoteService } from "./services/noteService";
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Layout } from "./components/layout/layout"
+import { Dashboard } from "./pages/dashboard"
+import { Notes } from "./pages/notes"
+import Profile from "./pages/profile"
+import Settings from "./pages/settings"
 
 function App() {
-  // const noteData = new NoteService().getData();
-  return (
-    <Layout>
-      <div className="App">App</div>
-    </Layout>
-  );
+	// const noteData = new NoteService().getData();
+	return (
+		<Layout>
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/notes' element={<Notes />} />
+				<Route path='/settings' element={<Settings />} />
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+		</Layout>
+	)
 }
 
-export default App;
+export default App
