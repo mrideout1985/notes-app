c9ab0b19d6f0646ccdb3c807930cab3503e13bab
mrideout1985 <mrideout1985@gmail.com>
2022-03-10 04:23:15 +0000

    refactored code, fixed useEffect. :raised_hands:

--- front-end/src/stores/authProvider.tsx
+++ front-end/src/stores/authProvider.tsx
@@ -6,39 +6,41 @@ const AuthContext = createContext<any>(null)
 interface AuthProviderInterface {
 	children: React.ReactNode
 }
+const setLocalStorage = (key: any, value: any) => {
+	try {
+		window.localStorage.setItem(key, JSON.stringify(value))
+	} catch (e) {
+		console.log(e)
+	}
+}
+const getLocalStorage = (key: any, initialValue: any) => {
+	try {
+		const value = window.localStorage.getItem(key)
+		return value ? JSON.parse(value) : initialValue
+	} catch (e) {
+		return initialValue
+	}
+}
 
 export const AuthProvider = ({ children }: AuthProviderInterface) => {
 	const [user, setUser] = useState(() => getLocalStorage("user", null))
 
-	function getLocalStorage(key: any, initialValue: any) {
-		try {
-			const value = window.localStorage.getItem(key)
-			return value ? JSON.parse(value) : initialValue
-		} catch (e) {
-			// if error, return initial value
-			return initialValue
-		}
-	}
-	function setLocalStorage(key: any, value: any) {
-		try {
-			window.localStorage.setItem(key, JSON.stringify(value))
-		} catch (e) {
-			// catch possible errors:
-			// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
-		}
-	}
 	useEffect(() => {
-		userService.getLoggedInUser().then(res => {
-			setUser(res)
-			setLocalStorage("user", user)
-		})
-	}, [])
+		// userService.getLoggedInUser().then(res => {
+		// 	setUser(res.email)
+		// 	setLocalStorage("user", user)
+		// })
+		setLocalStorage("user", user)
 
-	const userLogout = () => {
-		userService.logout()
-		setUser(null)
-	}
-	const providerValue = useMemo(() => ({ user, setUser, userLogout }), [user])
+		const cleanup = () => {
+			if (user === "") {
+				setUser(null)
+			}
+		}
+		cleanup()
+	}, [user])
+
+	const providerValue = useMemo(() => ({ user, setUser }), [user])
 
 	return (
 		<AuthContext.Provider value={providerValue}>
