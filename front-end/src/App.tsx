8f332bdef22446c4f0b1d1e52f561db38257ee1c
mrideout1985 <mrideout1985@gmail.com>
2022-03-10 04:20:46 +0000

    unwrapped paths from protected route for now :wip:

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,27 +1,20 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import { Layout } from "./components/layout/layout"
-import { Login } from "./pages/login"
 import { Notes } from "./pages/notes"
-import { Register } from "./pages/register"
 import { Profile } from "./pages/profile"
 import { RequireAuth } from "./components/requireAuth/requireAuth"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useAuth } from "./hooks/useAuth"
 
 function App() {
 	return (
 		<>
 			<Layout>
 				<Routes>
-					{/* Public Routes */}
-					<Route path='login' element={<Login />} />
-					<Route path='register' element={<Register />} />
 					{/* Protected Routes */}
-					<Route element={<RequireAuth />}>
-						<Route path='profile' element={<Profile />} />
-						<Route path='notes' element={<Notes />} />
-					</Route>
+					<Route path='profile' element={<Profile />} />
+					<Route path='notes' element={<Notes />} />
+					<Route element={<RequireAuth />}></Route>
 					{/* Catch All */}
 				</Routes>
 			</Layout>
