2a79c7f11bba7724de3bc3118be54677f358e072
mrideout1985 <mrideout1985@gmail.com>
2022-03-23 20:09:19 +0000

    unrwapped protected routes due to errors. wip

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,15 +5,18 @@ import { Notes } from "./pages/notes"
 import { Profile } from "./pages/profile"
 import { RequireAuth } from "./components/requireAuth/requireAuth"
 import "bootstrap/dist/css/bootstrap.min.css"
+// import { Unauthorized } from "./pages/unauthorized"
 
 function App() {
 	return (
 		<>
 			<Layout>
 				<Routes>
-					{/* Protected Routes */}
+					{/* <Route path='unauthorized' element={<Unauthorized />} /> */}
 					<Route path='profile' element={<Profile />} />
 					<Route path='notes' element={<Notes />} />
+
+					{/* Protected Routes */}
 					<Route element={<RequireAuth />}></Route>
 					{/* Catch All */}
 				</Routes>
