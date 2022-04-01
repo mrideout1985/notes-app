cb12e54b3a59b3bb8338314f879fcda320338804
mrideout1985 <mrideout1985@gmail.com>
2022-04-01 23:28:34 +0100

    fixed issue with user logged in 1st render.

--- front-end/src/stores/authProvider.tsx
+++ front-end/src/stores/authProvider.tsx
@@ -23,12 +23,8 @@ const getLocalStorage = (key: any, initialValue: any) => {
 
 export const AuthProvider = ({ children }: AuthProviderInterface) => {
 	const [user, setUser] = useState(() => getLocalStorage("user", null))
-
 	useEffect(() => {
 		setLocalStorage("user", user)
-		if (user === "") {
-			setUser(null)
-		}
 	}, [user])
 
 	const providerValue = useMemo(() => ({ user, setUser }), [user])
