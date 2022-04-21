c774ed2c16f73056fdd090a8ee7de2200e2f2988
mrideout1985 <mrideout1985@gmail.com>
2022-04-21 01:07:47 +0100

    refactoring

--- front-end/src/components/form/__tests__/input.test.tsx
+++ front-end/src/components/form/__tests__/input.test.tsx
@@ -4,7 +4,7 @@ import { fireEvent, render, screen } from "@testing-library/react"
 import { FormProvider, useForm } from "react-hook-form"
 import { Input } from "../input"
 
-const defaultValue = { title: "", description: "" }
+const defaultValue = { title: "cockring", description: "cockring" }
 
 describe("<Input />", () => {
 	it("Input should render", () => {
@@ -28,6 +28,7 @@ describe("<Input />", () => {
 	it("should be able to type into input field", () => {
 		const TestInput = () => {
 			const methods = useForm()
+
 			return (
 				<FormProvider {...methods}>
 					<Input
@@ -45,5 +46,82 @@ describe("<Input />", () => {
 		const inputElement = screen.getByRole("textbox", { name: "MyInput" })
 		fireEvent.change(inputElement, { target: { value: "testytesterson" } })
 		expect(inputElement).toHaveValue("testytesterson")
+	})
+
+	it("should have correct type", () => {
+		const TestInput = () => {
+			const methods = useForm()
+
+			return (
+				<FormProvider {...methods}>
+					<Input
+						id='title'
+						label='MyInput'
+						type='text'
+						fieldName='title'
+						defaultValue={defaultValue}
+						placeHolder='title'
+					/>
+				</FormProvider>
+			)
+		}
+
+		render(<TestInput />)
+
+		const inputElement = screen.getByRole("textbox", { name: "MyInput" })
+
+		fireEvent.change(inputElement, { target: { value: "testytesterson" } })
+
+		expect(inputElement).toHaveAttribute("type", "text")
+	})
+
+	it("should have a working label", () => {
+		const TestInput = () => {
+			const methods = useForm()
+
+			return (
+				<FormProvider {...methods}>
+					<Input
+						id='title'
+						label='MyInput'
+						type='text'
+						fieldName='title'
+						defaultValue={defaultValue}
+						placeHolder='title'
+					/>
+				</FormProvider>
+			)
+		}
+
+		render(<TestInput />)
+
+		expect(screen.getByLabelText("MyInput")).toBeTruthy()
+	})
+
+	it("should have correct defaultValue", () => {
+		const TestInput = () => {
+			const methods = useForm()
+
+			return (
+				<FormProvider {...methods}>
+					<Input
+						id='title'
+						label='MyInput'
+						type='text'
+						fieldName='title'
+						defaultValue={defaultValue}
+						placeHolder='title'
+					/>
+				</FormProvider>
+			)
+		}
+
+		render(<TestInput />)
+
+		expect(screen.getByLabelText("MyInput")).toHaveValue("cockring")
+	})
+
+	it("should show errors", () => {
+		// check error text and classes
 	})
 })
