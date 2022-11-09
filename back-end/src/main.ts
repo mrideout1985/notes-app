2f11593a0a79913378e0a6f8a24ad3f3d3ca45c6
mrideout1985 <mrideout1985@gmail.com>
2022-11-09 23:57:30 +0000

    asdf

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from "./modules/app.module"
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.use(cookieParser())
-	app.enableCors({ origin: "http://localhost:3001", credentials: true })
+	app.enableCors({ origin: "http://localhost:3001", credentials: true, allowedHeaders: ["Content-Type", "Allow", "Acess-Control-Allow-Origin", "Access-Control-Allow-Credentials"], })
 	await app.listen(3000)
 }
 bootstrap()
