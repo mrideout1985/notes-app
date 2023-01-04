e3aff5b4355c6ee484c95b327807172825deea51
Matt Rideout <30986733+mrideout1985@users.noreply.github.com>
2023-01-04 23:29:27 +0000

    updated vite and server script

--- start-servers.sh
+++ start-servers.sh
@@ -1,6 +1,6 @@
 # Start the front-end server in a new tab using the Bash profile
-wt new-tab --bash --profile Basher bash -ic "$(echo -e 'cd front-end\nnpm run dev')"
-wt new-tab --bash --profile Basher bash -ic "$(echo -e 'cd back-end\nnpm run start:dev')"
+wt new-tab --bash --profile Bash bash -ic "$(echo -e 'cd front-end\nnpm run dev')"
+wt new-tab --bash --profile Bash bash -ic "$(echo -e 'cd back-end\nnpm run start:dev')"
 
 
 
