53ac391c55485fb1501a04b225bff2221096628f
Matt Rideout <30986733+mrideout1985@users.noreply.github.com>
2023-01-07 13:04:12 +0000

    feature: added functionality to create user article wip

--- front-end/src/api/services/services.ts
+++ front-end/src/api/services/services.ts
@@ -1,73 +1,97 @@
-import axios from "axios"
+import axios from 'axios'
 
 export const register = async (email: string, password: string) => {
-	const response = axios
-		.post(
-			"http://localhost:3000/auth/register",
-			{
-				email,
-				password,
-			},
-			{
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		)
-		.catch(function (error) {
-			if (error) {
-				return error
-			}
-		})
+   const response = axios
+      .post(
+         'http://localhost:3000/auth/register',
+         {
+            email,
+            password,
+         },
+         {
+            headers: {
+               'Content-Type': 'application/json',
+            },
+         },
+      )
+      .catch(function (error) {
+         if (error) {
+            return error
+         }
+      })
 
-	return await response
+   return await response
 }
 
 export const login = async (email: string, password: string) => {
-	const response = axios
-		.post(
-			`http://localhost:3000/auth/login`,
-			{
-				email,
-				password,
-			},
-			{
-				withCredentials: true,
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		)
-		.then(res => {
-			if (res) {
-				return res
-			}
-		})
-		.catch(error => {
-			if (error) {
-				return error
-			}
-		})
+   const response = axios
+      .post(
+         `http://localhost:3000/auth/login`,
+         {
+            email,
+            password,
+         },
+         {
+            withCredentials: true,
+            headers: {
+               'Content-Type': 'application/json',
+            },
+         },
+      )
+      .then((res) => {
+         if (res) {
+            return res
+         }
+      })
+      .catch((error) => {
+         if (error) {
+            return error
+         }
+      })
 
-	return await response
+   return await response
 }
 
 export const logout = async () => {
-	sessionStorage.clear()
-	localStorage.clear()
-	return await fetch("http://localhost:3000/auth/logout", {
-		method: "POST",
-		credentials: "include",
-	})
+   sessionStorage.clear()
+   localStorage.clear()
+   return await fetch('http://localhost:3000/auth/logout', {
+      method: 'POST',
+      credentials: 'include',
+   })
 }
 
 export const getAndSetLoggedInUser = async (token: string | null) => {
-	const response = axios.get("http://localhost:3000/articles/my-articles", {
-		withCredentials: true,
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: "Bearer " + token,
-		},
-	})
-	return await response
+   const response = axios.get('http://localhost:3000/articles/my-articles', {
+      withCredentials: true,
+      headers: {
+         'Content-Type': 'application/json',
+         Authorization: 'Bearer ' + token,
+      },
+   })
+   return await response
+}
+
+export const createArticle = async (
+   data: any,
+   token: any,
+   userId: number | undefined,
+) => {
+   const response = axios.post(
+      'http://localhost:3000/articles',
+      {
+         title: data.title,
+         description: data.description,
+         body: data.body,
+         authorId: userId,
+      },
+      {
+         withCredentials: true,
+         headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+         },
+      },
+   )
+   return await response
 }
--- front-end/src/pages/Notes.tsx
+++ front-end/src/pages/Notes.tsx
@@ -1,34 +1,68 @@
-import useGetUserNotes, { Data } from "../api/hooks/getUserNotes";
+import { Controller, useFormContext } from 'react-hook-form'
+import { Form, FormGroup, Input, Label } from 'reactstrap'
+
+import { createArticle } from '@/api/services/services'
+import useUserStore from '@/stores/authstore'
 
 const Notes = () => {
-  const userNotes = useGetUserNotes();
+   const { control, handleSubmit } = useFormContext()
+   const user = useUserStore()
+   const token = localStorage.getItem('token')
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        gap: "1rem",
-        flexWrap: "wrap",
-      }}
-    >
-      {userNotes?.data?.map((note: Data) => {
-        return (
-          <div
-            key={note.id}
-            style={{
-              width: "200px",
-              height: "200px",
-              fontSize: "10px",
-              backgroundColor: "darkgray",
-            }}
-          >
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+   const onSubmit = handleSubmit((data) => {
+      createArticle(data, token, user.currentUser?.id).then((res) => {
+         console.log(res)
+      })
+   })
 
-export default Notes;
+   return (
+      <div
+         style={{
+            display: 'flex',
+            gap: '1rem',
+            flexWrap: 'wrap',
+         }}
+      >
+         <Form onSubmit={onSubmit}>
+            <FormGroup>
+               <Controller
+                  name="title"
+                  control={control}
+                  render={({ field }) => (
+                     <>
+                        <Label htmlFor="title">title</Label>
+                        <Input id="title" type="text" {...field} />
+                     </>
+                  )}
+                  defaultValue=""
+               />
+               <Controller
+                  name="description"
+                  control={control}
+                  render={({ field }) => (
+                     <>
+                        <Label htmlFor="description">description</Label>
+                        <Input id="description" type="text" {...field} />
+                     </>
+                  )}
+                  defaultValue=""
+               />
+               <Controller
+                  name="body"
+                  control={control}
+                  render={({ field }) => (
+                     <>
+                        <Label htmlFor="body">body</Label>
+                        <Input id="body" type="text" {...field} />
+                     </>
+                  )}
+                  defaultValue=""
+               />
+               <Input type="submit" />
+            </FormGroup>
+         </Form>
+      </div>
+   )
+}
+
+export default Notes
