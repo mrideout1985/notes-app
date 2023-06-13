9e8fd202f584535fa5f8db38c9301bb6e00c9bf1
mrideout1985 <mrideout1985@gmail.com>
2023-06-13 13:01:59 +0100

    removed bootstrap

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,6 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import 'open-props/normalize'
 import 'open-props/style'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
